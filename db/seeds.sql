3f6c0713e1fbea9bd0e2b3e5c4a80a2da3334cee
bindi-v <bindi.vaghela@gmail.com>
2021-10-18 19:12:57 -0400

    create seeds.sql and added data

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (id, department_name)
+VALUES (1,"Finance"),
+       (2, "Information Technology"),
+       (3, "Sales"),
+       (4, "Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Person", 80, 3)
+        ("Lead Engineer", 150, 2)
+        ("Software Engineer", 120, 2)
+        ("Account Manager", 160, 1)
+        ("Legal Team Lead", 250, 4)
+        ("Accountant", 125, 1)
+        ("Lawyer", 190, 4)
+        ("Sales Team Lead", 110, 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Mike", "Chan", 3)
+       ("Ashley", "Rodriguez", 2)
+       ("Kevin", "Tupik", 2)
+       ("Kunal", "Singh", 1)
+       ("Malia", "Brown", 1)
+       ("Sarah", "Lourd", 4)
+       ("Tom", "Allen", 4)
+       ("John", "Doe", 3);
