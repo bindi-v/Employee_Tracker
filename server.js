409439239428588e9bc2b6a64f602aa937bb57c2
bindi-v <bindi.vaghela@gmail.com>
2021-10-18 21:36:09 -0400

    added function to view all departments

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
+const { start } = require('repl');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,3 +23,59 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+db.connect(function(err){
+    if(err) throw err;
+    start()
+});
+
+function start(){
+    inquirer.prompt([
+        {
+       type: 'list',
+       name: 'action',
+       message: "What would you like to do?",
+       choices: [
+           'View all Departments',
+           'View all Roles',
+           'View all Employees',
+           'Add a Department',
+           'Add a Role',
+           'Add an Employee',
+           "Update an Employee's Role",
+           'Exit'
+       ]
+    }
+])
+.then(function(answer){
+    if(answer.action === 'View all Departments'){
+        viewDepartments();
+    }else if(answer.action === 'View all Roles'){
+        viewRoles();
+    }else if(answer.action === 'View all Employees'){
+        viewEmployees();
+    }else if (answer.action === 'Add a Department'){
+        addDepartment();
+    }else if(answer.action === 'Add a Role'){
+        addRole();
+    }else if(answer.action === 'Add an Employee'){
+        addEmployee();
+
+    }else if(answer.action === "Update an Employee's Role"){
+        updateRole();
+    }else if(answer.action === 'Exit'){
+        db.end();
+    }
+})
+}
+
+function viewDepartments() {
+    const sql = "SELECT * FROM department";
+    db.query(sql, function(err, res){
+        console.log('DEPARTMENTS:');
+        res.forEach(department => {
+            console.log(`ID: ${department.id} | Name: ${department.name}`)
+        })
+        start();
+    });
+};
